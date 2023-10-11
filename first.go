c3483cc7a474b0a0924bd4bccc9b04800c8a8a11
DariaBristol <101260296+DariaBristol@users.noreply.github.com>
2023-10-11 19:33:08 +0100

    Update first.go

--- first.go
+++ first.go
@@ -15,38 +15,36 @@ import "math"
 import "strconv"
 
 type dots struct {
-	x int
-	y int
+	x    int
+	y    int
 	name string
 }
 
-
-
-
 func work() {
 	//const power float64 = math.Pow( 10,6)
-	newdots :=[1000000]dots
-	var stringjoint string= " "
-	var stringdistant string= " "
-	for _, pnt := range newdots {
-		fmt.Scanf("%s &d %d", &pnt.name, &pnt.x, &pnt.y)
+	var newdots [3]dots
+	var stringjoint string = " "
+	var stringdistant string = " "
+	for i := 0; i < 3; i++ {
+		fmt.Scanln("%s %d %d", &newdots[i].name, &newdots[i].x, &newdots[i].y)
 	}
 	//i := 0
 	//newdots := [const a  = (math.Pow( 10,6))]dots
-	for  i := 0; i <= int(math.Pow( 10,6)); i++ {
+	//for i := 0; i <= int(math.Pow(10, 6)); i++ {
+	for i := 0; i < 3; i++ {
 		switch newdots[i].name {
 		case "standard":
-			fmt.Println(math.Pow(float64 (newdots[i].x), 2) + math.Pow(float64 (newdots[i].y), 2))
+			fmt.Println(math.Pow(float64(newdots[i].x), 2) + math.Pow(float64(newdots[i].y), 2))
 
 		case "joint":
-			stringjoint = strconv.Itoa(int (math.Atan2(float64 (newdots[i].y), float64 (newdots[i].x))))
+			stringjoint = strconv.Itoa(int(math.Atan2(float64(newdots[i].y), float64(newdots[i].x))))
 			//fmt.Println(math.Atan2(float64 (newdots[i].y), float64 (newdots[i].x)))
 			fallthrough
 
 		case "distant":
 
-			stringdistant =strconv.Itoa((newdots[i].x + newdots[i].y) + 1/(newdots[i].x + newdots[i].y) )
-fmt.Println(stringdistant + stringjoint)
+			stringdistant = strconv.Itoa((newdots[i].x + newdots[i].y) + 1/(newdots[i].x+newdots[i].y))
+			fmt.Println(stringdistant + stringjoint)
 		}
 	}
 }
